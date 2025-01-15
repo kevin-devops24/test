eddcf43d93bfaed233ce3ac8bce55ea7a2da9476
kevin-devops24 <kevin@bae.gg>
2025-01-15 21:09:39 +0100

    Delete CalculatorFX directory

--- CalculatorFX/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.calculatorfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.calculatorfx to javafx.fxml;
-    exports org.example.calculatorfx;
-}
